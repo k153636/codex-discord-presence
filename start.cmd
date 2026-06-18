49703ef9370452ef701d23c3ec0a9216a6f76ef6
k153636 <o-felice@outlook.jp>
2026-06-18 16:54:49 +0900

    Always rebuild before tray launch

--- start.cmd
+++ start.cmd
@@ -4,10 +4,8 @@ set "ROOT_DIR=%~dp0"
 set "APP_DIR=%ROOT_DIR%publish"
 set "APP_EXE=%APP_DIR%\discord-presence-for-codex.exe"
 
-if not exist "%APP_EXE%" (
-  call "%ROOT_DIR%build.cmd"
-  if errorlevel 1 exit /b 1
-)
+call "%ROOT_DIR%build.cmd"
+if errorlevel 1 exit /b 1
 
 powershell -NoProfile -ExecutionPolicy Bypass -Command "Start-Process -FilePath '%APP_EXE%' -ArgumentList '--project','%ROOT_DIR%' -WorkingDirectory '%ROOT_DIR%' -WindowStyle Hidden"
 echo Started Codex Discord RPC.
